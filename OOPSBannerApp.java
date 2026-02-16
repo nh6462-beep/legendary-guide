4a38b8a27db0f1b6f1d3f136243fbc4e5a2b8c89
Holkunde Nikhil Pravin <nh6462@srmist.edu.in>
2026-02-16 13:49:28 +0530

    UC1: Implement Print OOPS feature - Print literal text OOPS to console using Java fundamentals

---
+++ OOPSBannerApp.java
@@ -0,0 +1,27 @@
+/**
+ * OOPS Banner App - Use Case 1 (UC1): Print OOPS
+ * 
+ * This is the foundational Java class for the OOPS Banner App project.
+ * It demonstrates the basic concepts of Java programming including:
+ * - Class as a container for data and logic
+ * - The main() method as the application's entry point
+ * - Static keyword usage
+ * - String literals
+ * - Method invocation via System.out.println()
+ * 
+ * @author Holkunde Nikhil Pravin
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+    
+    /**
+     * Main method - Entry point of the application
+     * This is where the program execution begins
+     * 
+     * @param args Command-line arguments (not used in UC1)
+     */
+    public static void main(String[] args) {
+        // Print the literal text "OOPS" to the console
+        System.out.println("OOPS");
+    }
+}
